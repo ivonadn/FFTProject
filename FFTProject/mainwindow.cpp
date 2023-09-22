1c3bf862bd1f99b46cc23eaf52b6c30736d6fb42
ivonadn <106910809+ivonadn@users.noreply.github.com>
2023-09-22 19:23:49 +0300

    Update mainwindow.cpp

--- FFTProject/mainwindow.cpp
+++ FFTProject/mainwindow.cpp
@@ -39,6 +39,34 @@ void MainWindow::on_pushButton_clicked()
     }
 }
 
+void fftshift(fftw_complex* data, int width, int height) {
+    int halfWidth = width / 2;
+    int halfHeight = height / 2;
+
+    // Create a temporary array to hold the shifted values
+    fftw_complex* temp = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * width * height);
+
+    // Shift quadrants
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int shiftedY = (y + halfHeight) % height;
+            int shiftedX = (x + halfWidth) % width;
+            temp[shiftedY * width + shiftedX][0] = data[y * width + x][0];
+            temp[shiftedY * width + shiftedX][1] = data[y * width + x][1];
+        }
+    }
+
+    // Copy back to the original array
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            data[y * width + x][0] = temp[y * width + x][0];
+            data[y * width + x][1] = temp[y * width + x][1];
+        }
+    }
+
+    fftw_free(temp);
+}
+
 
 QImage blurChannel(const QImage &channelImage) {
     int width = channelImage.width();
@@ -60,34 +88,15 @@ QImage blurChannel(const QImage &channelImage) {
     fftw_execute(forwardPlan);
     fftw_destroy_plan(forwardPlan);
 
-    //this returns a black image
-//    double cutoff = 0.1;
-//    for (int y = 0; y < height; y++) {
-//        for (int x = 0; x < width; x++) {
-//            double dist = sqrt((x - width / 2) * (x - width / 2) + (y - height / 2) * (y - height / 2));
-//            if (dist > cutoff) {
-//                out[y * width + x][0] = 0.0;
-//                out[y * width + x][1] = 0.0;
-//            }
-//        }
-//    }
-
-    // Low-pass filter
-    int percent = 50;
-    int s = 5;
-    int cX = width/2;
-    int cY = height/2;
-
-    qDebug() <<"TEST "<<((double)percent/(double)100)*height;
-
-    for(int i = 0; i<(width*height);i++){
-        int row = i/s;
-        int col = i%s;
-        int distance = sqrt((col-cX)*(col-cX)+(row-cY)*(row-cY));
-        double min = std::min(cX,cY);
-        if(distance<((double)percent/(double)100)*min){
-            out[i][0] = 0.0;
-            out[i][1] = 0.0;
+    //Low-pass filter
+    double cutoff = 600;
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            double dist = sqrt(x * x + y * y);  // Distance from the top-left corner
+            if (dist > width/2) {
+                out[y * width + x][0] = 0.0;
+                out[y * width + x][1] = 0.0;
+            }
         }
     }
 
@@ -152,6 +161,7 @@ QImage sharpenChannel(const QImage &channelImage) {
     int width = channelImage.width();
     int height = channelImage.height();
 
+    //2d array of the image, one for the fft forward and one for the inverse
     fftw_complex *in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * width * height);
     fftw_complex *out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * width * height);
 
@@ -168,12 +178,11 @@ QImage sharpenChannel(const QImage &channelImage) {
     fftw_execute(forwardPlan);
     fftw_destroy_plan(forwardPlan);
 
-    // High-pass filter
-    //the higher cutoff value, the higher sharpening effect
-    double cutoff = 200.0;
+    //High-pass filter
+    double cutoff = 60;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            double dist = sqrt((x - width / 2) * (x - width / 2) + (y - height / 2) * (y - height / 2));
+            double dist = sqrt(x * x + y * y);  // Distance from the top-left corner
             if (dist < cutoff) {
                 out[y * width + x][0] = 0.0;
                 out[y * width + x][1] = 0.0;
@@ -267,10 +276,11 @@ void MainWindow::on_pushButton_2_clicked()
         return;
     }
 
-    //QImage grayImage = convertToGrayScale(originalImage);
+    QImage grayImage = convertToGrayScale(originalImage);
 
-    // Perform FFT and apply Gaussian blur using FFTW (You should implement this function)
+    //QImage blurredImage = blurChannel(grayImage);
     QImage blurredImage = blur(originalImage);
+
     int width = ui->label_blur->width();
     int height = ui->label_blur->height();
     QPixmap pix = QPixmap::fromImage(blurredImage);
@@ -287,6 +297,9 @@ void MainWindow::on_pushButton_3_clicked()
         return;
     }
 
+    QImage grayImage = convertToGrayScale(originalImage);
+
+    //QImage sharpenedImage = sharpenChannel(grayImage);
     QImage sharpenedImage = sharpen(originalImage);
     int width = ui->label_sharpen->width();
     int height = ui->label_sharpen->height();
