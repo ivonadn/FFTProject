c47b8ccaf03ad292fe627bfa7e198e6b34422470
ivonadn <ivona.nencheva@gmail.com>
2023-09-22 12:44:56 +0300

    Update ui_mainwindow.h

--- build-FFTProject-Desktop_Qt_6_3_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-FFTProject-Desktop_Qt_6_3_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -26,10 +26,13 @@ public:
     QWidget *centralwidget;
     QPushButton *pushButton;
     QLabel *label;
-    QLabel *label_2;
+    QLabel *label_loaded;
     QPushButton *pushButton_2;
-    QLabel *label_3;
+    QLabel *label_sharpen;
     QLabel *label_4;
+    QLabel *label_5;
+    QPushButton *pushButton_3;
+    QLabel *label_blur;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -37,31 +40,40 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(1017, 686);
+        MainWindow->resize(1543, 686);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(140, 30, 80, 24));
+        pushButton->setGeometry(QRect(230, 30, 80, 24));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
         label->setGeometry(QRect(230, 100, 111, 16));
-        label_2 = new QLabel(centralwidget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(40, 120, 441, 401));
+        label_loaded = new QLabel(centralwidget);
+        label_loaded->setObjectName(QString::fromUtf8("label_loaded"));
+        label_loaded->setGeometry(QRect(40, 120, 441, 401));
         pushButton_2 = new QPushButton(centralwidget);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setGeometry(QRect(320, 30, 111, 24));
-        label_3 = new QLabel(centralwidget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
-        label_3->setGeometry(QRect(550, 120, 441, 401));
+        pushButton_2->setGeometry(QRect(1230, 50, 111, 24));
+        label_sharpen = new QLabel(centralwidget);
+        label_sharpen->setObjectName(QString::fromUtf8("label_sharpen"));
+        label_sharpen->setGeometry(QRect(550, 120, 441, 401));
         label_4 = new QLabel(centralwidget);
         label_4->setObjectName(QString::fromUtf8("label_4"));
-        label_4->setGeometry(QRect(690, 100, 91, 16));
+        label_4->setGeometry(QRect(690, 100, 101, 16));
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(1250, 100, 111, 16));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(690, 40, 80, 24));
+        label_blur = new QLabel(centralwidget);
+        label_blur->setObjectName(QString::fromUtf8("label_blur"));
+        label_blur->setGeometry(QRect(1050, 120, 441, 401));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 1017, 21));
+        menubar->setGeometry(QRect(0, 0, 1543, 21));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -77,10 +89,13 @@ public:
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
         pushButton->setText(QCoreApplication::translate("MainWindow", "Browse", nullptr));
         label->setText(QCoreApplication::translate("MainWindow", "Loaded image", nullptr));
-        label_2->setText(QString());
+        label_loaded->setText(QString());
         pushButton_2->setText(QCoreApplication::translate("MainWindow", "Gaussian Blur", nullptr));
-        label_3->setText(QString());
-        label_4->setText(QCoreApplication::translate("MainWindow", "Blurred image", nullptr));
+        label_sharpen->setText(QString());
+        label_4->setText(QCoreApplication::translate("MainWindow", "Sharpened image", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "Blurred image", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Sharpen", nullptr));
+        label_blur->setText(QString());
     } // retranslateUi
 
 };
